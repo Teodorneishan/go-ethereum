314448b71f2d5d02bc2cf20c57acbe6ce550336b
Teodorneishan <56262587+Teodorneishan@users.noreply.github.com>
2024-06-15 21:35:49 +0300

    Update my-tests.js

--- hardhat/test/my-tests.js
+++ hardhat/test/my-tests.js
@@ -1,18 +1,17 @@
 const { expect } = require("chai");
-const hre = require("hardhat");
-const { time } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
+const { ethers } = require("hardhat");
+const { time } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("Lock", function () {
   it("Should set the right unlockTime", async function () {
-    const lockedAmount = 1_000_000_000;
+    const lockedAmount = ethers.utils.parseEther("1.0"); // 1 ether
     const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
     const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
 
-    // deploy a lock contract where funds can be withdrawn
-    // one year in the future
-    const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-      value: lockedAmount,
-    });
+    // deploy a lock contract where funds can be withdrawn one year in the future
+    const Lock = await ethers.getContractFactory("Lock");
+    const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+    await lock.deployed();
 
     // assert that the value is correct
     expect(await lock.unlockTime()).to.equal(unlockTime);
