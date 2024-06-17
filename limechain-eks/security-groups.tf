cdc6dc64e7a543ce3d8f3ec4137c722d230ce03e
Teodor <teodorneishan@gmail.com>
2024-06-17 10:43:32 +0300

    try to add clusterip service - appears to be nodeport

--- limechain-eks/security-groups.tf
+++ limechain-eks/security-groups.tf
@@ -11,27 +11,6 @@ resource "aws_security_group" "worker_group_one" {
       "10.0.0.0/16"
     ]
   }
-  ingress {
-    from_port = 30304
-    to_port   = 30304
-    protocol  = "tcp"
-    cidr_blocks = ["10.0.0.0/16"] #
-  }
-
-  ingress {
-    from_port = 8552
-    to_port   = 8552
-    protocol  = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  ingress {
-    from_port = 8553
-    to_port   = 8553
-    protocol  = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
   egress {
     from_port = 0
     to_port   = 0
